e5c8af57a66b69536d4b99841031878784a71099
Marek Zdybel <marek@motta.com.pl>
2021-06-26 10:50:06 +0200

    Blog list and details page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path, include
+from django.contrib import admin
 """mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -18,4 +20,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls', namespace='blog')),
 ]
